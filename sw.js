1876ceb38944d7d814d31dc65104750fce9b67bc
Habeeb00 <habeebrahmanofficial@gmail.com>
2026-01-05 15:27:13 +0530

    touches

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching and share target routing
  */
 
-const CACHE_NAME = 'add2cal-v43';
+const CACHE_NAME = 'add2cal-v5';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
