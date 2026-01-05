625eec2f1d3cc9872b03e38975110ebbb314da69
Habeeb00 <habeebrahmanofficial@gmail.com>
2026-01-05 15:47:16 +0530

    vercel analytics

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Handles offline caching and share target routing
  */
 
-const CACHE_NAME = 'add2cal-v5';
+const CACHE_NAME = 'add2cal-v53';
 const ASSETS_TO_CACHE = [
     '/',
     '/index.html',
